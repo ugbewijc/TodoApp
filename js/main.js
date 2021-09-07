f74a4206882505b5feef24679887105ef72f2224
Ugbewi John Chioma <ugbewijc@gmail.com>
2021-09-07 18:46:54 +0100

    code refactoring

--- js/main.js
+++ js/main.js
@@ -1,77 +1,66 @@
-
-/* Js File for my TODO Apps
-by Ugbewi John Chioma
+/**
+Js File for my TODO Apps
+by John Chioma
 */
 
-const taskInputElement = document.getElementById('new-task'); // input Element
+const taskInputElement = document.getElementById('task'); // input Element
 const taskList = document.getElementById('tasklist'); // Task List
-// const list = document.querySelector('#tasklist');
 const errorElement = document.getElementById('input-error');
-// const addButton = document.getElementsByName('addButton');
-const addButton = document.getElementsByName('addButton')[0];
+//const addButton = document.getElementsByName('addButton')[0];
+const closeButton = document.getElementById('delete_task');
 
-// validate Task
-const validateTask = () => {
-  const patten = /^[\w ]+$/; // /^[A-Za-z0-9 ]+$/;
-  if (patten.test(taskInputElement.value) && taskInputElement.value !== '') {
-    errorElement.textContent = '';
-    addButton.removeAttribute('disabled');
-  } else {
-    errorElement.textContent = 'Invalid Input';
-    addButton.setAttribute('disabled', '');
-  }
-};
 
-/* input validation */
-taskInputElement.addEventListener('keydown', () => { // use focus or keydown
-  // setInterval(validateTask, 500);
-  setTimeout(validateTask, 100);
-});
-
-/* add input value to task list */
-addButton.addEventListener('click', (event) => {
-  const task = taskInputElement.value;
-  if (task === '') {
-    event.preventDefault();
-  }
-
+const createTask = (task) => {
   /* Create Elements needed */
-
-  // input
-  const input = document.createElement('input');
-  input.type = 'checkbox';
-  input.value = task;
-
-  // label
-  const label = document.createElement('label');
-  label.textContent = task;
-
   // list
   const list = document.createElement('li');
+  list.innerHTML= `
+  <input class="todo__task-checkbox" name="${task}" type="checkbox" value="${task}">
+  <label for="${task}">${task}</label>
+  <button id="delete_task"class="todo__delete-task" type="button" name="${task}">&times</button>
+  `;
 
-  /* Append Elements */
-  list.appendChild(input);
-  list.appendChild(label);
   // list first child
-  const listFirstChild = taskList.firstChild;
+  //const listFirstChild = taskList.firstChild;
   // Insert the new element before the first child
-  // taskList.appendChild(list);
-  taskList.insertBefore(list, listFirstChild);
+  taskList.appendChild(list);
+  //taskList.insertBefore(list, listFirstChild);
 
   /* Save task in arrary */
 
-  /* empty Add Task Input field */
+};
+
+const addTask = () => {
+  const task = taskInputElement.value;
+  if (task) {
+    createTask(task);
+  }
+
+  /** Empty Add Task Input field */
   taskInputElement.value = '';
-  addButton.setAttribute('disabled', '');
   taskInputElement.focus();
+};
+
+const removeHtmlElement = (htmlElement) => {
+  htmlElement.remove();
+};
+
+taskInputElement.addEventListener('keydown', (e) => {
+  if (e.key ==="Enter") {
+    addTask();
+  }
 });
 
-/* completed Task */
+/* completed Task
 taskList.addEventListener('click', (ev) => {
-  const elm = ev.target;
-  if (elm.tagName === 'INPUT') {
-  //  console.log(elm);
-    // elm.parentElement.classList.add('completed-task');
-    elm.parentElement.classList.toggle('completed-task');
-  }
-}, false);
+const elm = ev.target;
+if (elm.tagName === 'INPUT') {
+elm.parentElement.classList.toggle('completed-task');
+}
+}, false);*/
+
+document.addEventListener('click', (e) => {
+  if (e.target.id ==="delete_task") {
+    removeHtmlElement(e.target.parentElement);
+  };
+});
